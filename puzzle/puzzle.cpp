2bed0345dee6575f9c8677711358bca6157146c2
Yutaro Sugai <y.sugai@everyleaf.com>
2013-06-04 23:34:07 +0900

    Display 5x5 square

--- puzzle/puzzle.cpp
+++ puzzle/puzzle.cpp
@@ -3,8 +3,10 @@ using std::cin;
 using std::cout;
 
 int main(){
-  for(int hashNum = 1; hashNum <= 5; hashNum++) {
-    cout << "#";
+  for(int row = 1; row <= 5; row++){
+    for(int hashNum = 1; hashNum <= 5; hashNum++) {
+      cout << "#";
+    }
+    cout << "\n";
   }
-  cout << "\n";
 }
