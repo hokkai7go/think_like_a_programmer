1d29e4010ea4709ddb7ba48bc937bf69cf4e3b2b
hokkai7go <hokkai7go@gmail.com>
2013-06-30 23:59:07 +0900

    Extract function of double digit

--- puzzle/checksum.cpp
+++ puzzle/checksum.cpp
@@ -2,15 +2,26 @@
 using std::cin;
 using std::cout;
 
-int main(){
-  int digit;
-  cout << "Enter a single digit number, 0-9: ";
-  cin  >> digit;
+int doubleDigitValue(int digit){
   int doubledDigit = digit * 2;
   int sum;
 
-  if (doubledDigit >= 10) sum = 1 + doubledDigit % 10;
-    else sum = doubledDigit;
+  if (doubledDigit >= 10) {
+    sum = 1 + doubledDigit % 10;
+  } else {
+      sum = doubledDigit;
+  }
+  return sum;
+}
 
+int main(){
+  int digit;
+  int sum;
+  cout << "Enter a single digit number, 0-9: ";
+  cin  >> digit;
+
+  sum = doubleDigitValue(digit);
   cout << "Sum of digits in doubled number: " << sum << "\n";
 }
+
+
