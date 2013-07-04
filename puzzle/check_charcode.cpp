898f31b767128b4d73780ba884f38643d4bbc157
Yutaro Sugai <y.sugai@everyleaf.com>
2013-07-05 01:24:11 +0900

    Use >> instead of cin.get()

--- puzzle/check_charcode.cpp
+++ puzzle/check_charcode.cpp
@@ -6,8 +6,8 @@ int main(){
   char digit;
 
   cout << "Enter a one-digit number: ";
-  digit = cin.get();
+  cin >> digit;
 
-  int sum = digit;
+  int sum = digit - '0';
   cout << "Is the sum of digits " << sum << "? \n";
 }
